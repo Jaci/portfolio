1cdb0dc5c2c60ffd486abb1804d3dead95f5871e
Josh <jharris@peopleadmin.com>
2011-11-05 01:39:00 -0500

    removing jquery requirements due to error ActionView::Template::Error (jquery.js isnt precompiled)

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,6 @@ module Portfolio
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['application.js','jquery.min.js', 'application.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
