d91841cc19ccd8e98c06bcf520000bf20e12108b
Josh <jharris@peopleadmin.com>
2011-11-05 01:34:48 -0500

    adding config.assets.precompile

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module Portfolio
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['application.js','jquery.min.js', 'application.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
