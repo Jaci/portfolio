935fd652077f96586686f54bd84d621752a8e2d5
Josh <jharris@peopleadmin.com>
2011-11-08 00:34:49 -0600

    changing bucket name

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
     
   has_attached_file :project_pic_small, :styles => { :full => "150x200>" },
        :storage => :s3,
-       :bucket => 'jaci',
+       :bucket => 'jaci-harris',
        :s3_credentials => {
          :access_key_id => ENV['S3_KEY'],
          :secret_access_key => ENV['S3_SECRET']
