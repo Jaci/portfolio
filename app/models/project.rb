3edfbbb7ed3683fe1b419067872a92075b9e7594
Josh <jharris@peopleadmin.com>
2011-11-08 19:03:05 -0600

    taking away validation for content_type

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ActiveRecord::Base
-    attr_accessor :avatar
     
   has_attached_file :avatar, :styles => { :small => "150x200>" , :large => "250x500>"},
        :storage => :s3,
@@ -9,6 +8,5 @@ class Project < ActiveRecord::Base
          :secret_access_key => ENV['S3_SECRET']
        }
    validates_attachment_size :avatar, :less_than => 5.megabytes
-   validates_attachment_content_type :avatar, :content_type => [ 'image/jpeg', 'image/png', 'image/gif']
 
 end
